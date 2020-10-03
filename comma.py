59ea8528d50623176853af1cead8648386e014ab
madpad <madhu.rammoorthy@gmail.com>
2020-10-03 21:10:18 +0530

    corrected code

--- comma.py
+++ comma.py
@@ -2,7 +2,7 @@ spam = ['a','b','c','d','e']
 n=len(spam)
 last = spam[-1]
 newspam = spam
-newspam=spam.remove(last)
+newspam.remove(last)
 for i in range(n-1):
-	print(str(newspam[i]),end='')
-print(last)
+	print(newspam[i] + ',',end='')
+print('and ' + last)
