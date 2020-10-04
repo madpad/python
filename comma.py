8a6e5ec795cb12af9f593ccbb405c59b526bb0f8
madpad <madhu.rammoorthy@gmail.com>
2020-10-04 14:34:27 +0530

    modified with a customlist

--- comma.py
+++ comma.py
@@ -1,8 +1,14 @@
-spam = ['a','b','c','d','e']
+#spam = ['a','b','c','d','e']
+spam=[]
+while True:
+	element=input("enter an element in list or q to quit:")
+	spam=spam+[element]
+	if element=="q":
+		break
 n=len(spam)
 last = spam[-1]
 newspam = spam
-newspam.remove(last)
+newspam.pop() #pops out the last value in the list newspam
 for i in range(n-1):
 	print(newspam[i] + ',',end='')
 print('and ' + last)
